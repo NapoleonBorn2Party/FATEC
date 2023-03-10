862cd3c0c0a764d737bf8dccb6dd473ee6e42859
menonilongo <leo.p.longo@hotmail.com>
2023-03-10 19:22:05 -0300

    EP-A Invasão criado

---
+++ EP-A Invasão.py
@@ -0,0 +1,22 @@
+Após uma seleção muito rigorosa, você foi escolhido como o mais capacitado para a realização de uma tarefa de essencial importância para a continuidade da espécie humana.
+CONFIDENCIAL: A Terra será invadida por extraterrestres com a intenção de total extermínio de seus habitantes para futura ocupação do planeta. Descobrir a data dessa invasão é de vital importância para impedir tamanha destruição. Informantes alienígenas aliados conseguiram obter o códifo de ataque e as regras para sua decodificação. Sua tarefa será desenvolver um programa que leia o código secreto e o decodifique, determinanndo a data da invasão.
+
+### Mensagem dos Informantes
+O código é composto por três partes: a primeira parte são dois números naturais m e n, a segunda parte é formada por m números naturais e a terceira por n números naturais.
+Para determinar a data do evento, é necessário escolher o método correto: Método A ou Método B. Para saber o método adequado, existe o seguinte procedimento: (I) soma-se todos os m números, obtendo sm; (II) soma-se todos os dígitos de sm, obtendo sd; (III) se sd é par, aplica-se o Método A, caso contrário, aplica-se o Método B.
+
+### Método A
+Instruções: (I) ordena-se crescentemente os n números; (II) multiplica-se cada um dos n números por sua posição na sequência, considerando que o 1o item tem posição 1; (III) somam-se os n números já multiplicados, obtendo sn.
+
+### Método B
+Instruções: (I) soma-se a cada um dos n números o respectivo valor da sequência (1, 2, 4, 7, 11, 16, ...); (II) para cada n número já acrescido, tomam-se os dois dígitos mais à direita para serem acumulados; (III) o acúmulo total gera sn.
+
+### Descobrindo a data
+Descoberto o valor de sn, para determinar a data da invasão calcula-se o resto da divisão inteira de sn por 31, que representa o dia da invasão, adote zero como dia 31. De modo análogo, o resto da divisão inteira de sn por 12 indica o mês da invasão, o resto zero indicando dezembro. O ano, infelizmente, não pode ser descoberto.
+
+### Observações
+1) Faça a validação da entrada: (I) 0 < m < 6; (II) 0 < n < 11; (III) para todo x pertencente aos m números, 0 < x < 500.
+2) Não use funções/métodos pré-definidos da linguagem para ordenar ou somar os itens das listas.
+3) Considere que fevereiro possui 28 dias, pois os extraterrestres não atacam em anos bissextos.
+4) Caso o procedimento resulte em um dia que excede o máximo para o mês descoberto, deve-se supor que o código foi corrompido. Uma mensagem deve indicar essa situação!
+
