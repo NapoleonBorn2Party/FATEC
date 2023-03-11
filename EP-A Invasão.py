847d8d9236adadefcd80ce2e3d5b555ee1035466
NB2P <leo.p.longo@hotmail.com>
2023-03-11 19:24:07 -0300

    correção em entrada_m

--- EP-A Invasão.py
+++ EP-A Invasão.py
@@ -62,7 +62,7 @@ def entrada_m():
     m = input(int("m: "))
 
     while m > 6 and m < 0:
-        m = input(int("m deve estar no intervalo 0 < m < 6.\nm: "))
+        m = int(input("m deve estar no intervalo 0 < m < 6.\nm: "))
     
     return m
 
